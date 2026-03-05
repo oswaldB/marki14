458a02db64b32fb9d8d2e02ffadd8b9f2f5abe72
Oswald BERNARD <oswald.bernard@gmail.com>
2026-03-05 21:15:41 +0000

    clear the app.

--- app/__init__.py
+++
@@ -1,2 +0,0 @@
-# Package initialisation pour le dossier app
-# Cela permet à Python de traiter le dossier comme un package
--- app/static/js/stores/index.js
+++
@@ -1,5 +0,0 @@
-// stores/index.js - Main stores index
-// This file serves as an entry point for all Alpine.js stores
-
-// Import or reference all stores that need to be initialized
-console.log('Stores index loaded');
